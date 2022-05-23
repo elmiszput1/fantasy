8cb01720963468d066254cc28e58ddaa3f414d42
Ismail Musa <106095327+elmiszput1@users.noreply.github.com>
2022-05-23 15:51:33 +0100

    Add files via upload

---
+++ fantasy.php
@@ -0,0 +1,334 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <!--Matarial icon-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/fantasy5.css">
+    <title>Fantasy</title>
+</head>
+
+<body class="fantasy_body">
+    <div class="container">
+        <section class="row">
+            <div class="col-md-6 mt-md-5 p-5">
+                <div class="fantasy_fBold">Betensured</div>
+                <div class="fantasy_fSmall">Weekly Predictor</div>
+                <div class="fantasy_timerCountContainer">
+                    <div class="fantasy_timerCountKickOff">
+                        Kick Off
+                    </div>
+                    <div class="fantasy_timerCountTime">
+                        <div class="fantasy_mins">
+                            <div class="fantasy_minsCircle">
+                                07
+                            </div>
+                            <p>Hour</p>
+                        </div>
+                        <div class="fantasy_mins">
+                            <div class="fantasy_minsCircle">
+                                55
+                            </div>
+                            <p>Minutes</p>
+                        </div>
+                        <div class="fantasy_mins">
+                            <div class="fantasy_minsCircle">
+                                12
+                            </div>
+                            <p>Seconds</p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="fantasy_card fantasy_two">
+                    <div class="fantasy_twoCard">
+                        <img src="img/banner1.png">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    <section class="fantasy_sectionMiddle">
+        <div class="fantasy_sectionMiddleInner">
+            <div class="fantasy_sectionMiddleCard">
+                <div class="fantasy_secOne">
+                    <h3>Mon Apr 4 - 04:00PM</h3>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <div class="fantasy_secOneInner">
+                        <div class="fantasy_clLogo">
+                            <img src="img/chelsea.png">
+                        </div>
+                        <span>Chelsea</span>
+                    </div>
+
+                    <div class="fantasy_secOneInnerv">
+                        <span class="fantasy_vs">V</span>
+                    </div>
+
+                    <div class="fantasy_secOneInner">
+                        <div class="fantasy_clLogo">
+                            <img src="img/crystalpalace.png">
+                        </div>
+                        <span>Crystal Palace</span>
+                    </div>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <span class="fantasy_quest">
+                        <h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                            proident, sunt inow many reds exist in this match?</h4>
+                    </span>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <input class="fantasy_inputAns" type="text" name="answer" placeholder="Type answer">
+                </div>
+
+            </div>
+
+            <div class="fantasy_sectionMiddleCard">
+                <div class="fantasy_secOne">
+                    <h3>Mon Apr 4 - 04:00PM</h3>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <div class="fantasy_secOneInner">
+                        <div class="fantasy_clLogo">
+                            <img src="img/chelsea.png">
+                        </div>
+                        <span>Chelsea</span>
+                    </div>
+
+                    <div class="fantasy_secOneInnerv">
+                        <span class="fantasy_vs">V</span>
+                    </div>
+
+                    <div class="fantasy_secOneInner">
+                        <div class="fantasy_clLogo">
+                            <img src="img/crystalpalace.png">
+                        </div>
+                        <span>Crystal Palace</span>
+                    </div>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <span class="fantasy_quest">
+                        <h4>How many reds exist in this match?</h4>
+                    </span>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <input class="fantasy_inputAns" type="text" name="answer" placeholder="Type answer">
+                </div>
+
+            </div>
+
+            <div class="fantasy_sectionMiddleCard">
+                <div class="fantasy_secOne">
+                    <h3>Mon Apr 4 - 04:00PM</h3>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <div class="fantasy_secOneInner">
+                        <div class="fantasy_fantasy_clLogo">
+                            <img src="img/chelsea.png">
+                        </div>
+                        <span>Chelsea</span>
+                    </div>
+
+                    <div class="fantasy_secOneInnerv">
+                        <span class="vs">V</span>
+                    </div>
+
+                    <div class="fantasy_secOneInner">
+                        <div class="fantasy_clLogo">
+                            <img src="img/crystalpalace.png">
+                        </div>
+                        <span>Crystal Palace</span>
+                    </div>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <span class="fantasy_quest">
+                        <h4>How many reds exist in this match?</h4>
+                    </span>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <input class="fantasy_inputAns" type="text" name="answer" placeholder="Type answer">
+                </div>
+
+            </div>
+
+
+
+            <div class="fantasy_sectionMiddleCard">
+                <div class="fantasy_secOne">
+                    <h3>Mon Apr 4 - 04:00PM</h3>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <div class="fantasy_secOneInner">
+                        <div class="fantasy_clLogo">
+                            <img src="img/chelsea.png">
+                        </div>
+                        <span>Chelsea</span>
+                    </div>
+
+                    <div class="fantasy_secOneInnerv">
+                        <span class="fantasy_vs">V</span>
+                    </div>
+
+                    <div class="fantasy_secOneInner">
+                        <div class="fantasy_clLogo">
+                            <img src="img/crystalpalace.png">
+                        </div>
+                        <span>Crystal Palace</span>
+                    </div>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <span class="fantasy_quest">
+                        <h4>How many reds exist in this match?</h4>
+                    </span>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <input class="fantasy_inputAns" type="text" name="answer" placeholder="Type answer">
+                </div>
+
+            </div>
+
+            <div class="fantasy_sectionMiddleCard fantasy_tieBreaker">
+                <div class="fantasy_secOne">
+                    <h3>Mon Apr 4 - 04:00PM</h3>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <div class="fantasy_secOneInner">
+                        <div class="fantasy_clLogo">
+                            <img src="img/chelsea.png">
+                        </div>
+                        <span>Chelsea</span>
+                    </div>
+
+                    <div class="fantasy_secOneInnerv">
+                        <span class="fantasy_vs">V</span>
+                    </div>
+
+                    <div class="fantasy_secOneInner">
+                        <div class="fantasy_clLogo">
+                            <img src="img/crystalpalace.png">
+                        </div>
+                        <span>Crystal Palace</span>
+                    </div>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <span class="fantasy_quest">
+                        <h4>How many reds exist in this match?</h4>
+                    </span>
+                </div>
+                <div class="fantasy_secOneContainer">
+                    <input class="fantasy_inputAns" type="text" name="answer" placeholder="Type answer">
+                </div>
+                <div class="fantasy_tieBreakerwand">
+                    <div class="fantasy_tieBreakerwandInner">
+                        <span style="background-color:#161616; padding:6px;">Tie Breaker</span>
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+
+        <div class="container">
+            <div class="fantasy_sectionMiddBTN">
+                <button class="fantasy_btnDanger"> Submit</button>
+            </div>
+        </div>
+    </section>
+
+	<div class="container">
+		<section class="fantasy_sectionBottom">
+
+			<div class="fantasy_tbContainer">
+				<h2>Loaderboard</h2>
+				<div class="fantasy_tbContainerInner">
+					<div class="fantasy_tbContainerInnerPadd">
+						<div class="fantasy_tbHeader">
+							<button class="fantasy_btnLR">Last Round</button>
+							<button class="fantasy_btnLR">Over All</button>
+						</div>
+					</div>
+
+					<div class="fantasy_paddTable">
+						<table id="fantasy_customers">
+							<tr>
+								<th>Pos</th>
+								<th>Player Name</th>
+								<th>Crowns</th>
+								<th>PTS</th>
+							</tr>
+							<tr>
+								<td>1</td>
+								<td>Maria Akipelumi</td>
+								<td>1</td>
+								<td>100</td>
+							</tr>
+							<tr>
+								<td>2</td>
+								<td>Christina Fasola</td>
+								<td>-</td>
+								<td>20</td>
+							</tr>
+							<tr>
+								<td>3</td>
+								<td>Uchenna M </td>
+								<td>-</td>
+								<td>60</td>
+							</tr>
+							<tr>
+								<td>4</td>
+								<td>Roland Oshodi</td>
+								<td>1</td>
+								<td>100</td>
+							</tr>
+
+						</table>
+					</div>
+
+				</div>
+
+			</div>
+		</section>
+    </div>
+
+    <!-- PopUP -->
+    <div id="fantasy_popup_con">
+
+        <div class="fantasy_popInner">
+            <div class="fantasy_closeHeader">
+                <span>
+                    <span class="material-icons-sharp">close</span>
+                </span>
+            </div>
+            <div class="fantasy_popBody">
+                <div class="fantasy_circleBody">
+                    <span><img src="img/rocket-cl.png"></span>
+                </div>
+            </div>
+            <div class="fantasy_succ">
+                <h3>Suuuuuuuuu</h3>
+            </div>
+
+            <div class="fantasy_sectionMiddBTN">
+                <button class="fantasy_btnDanger"> Submit</button>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
